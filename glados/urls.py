330a1dfea0e9541240a3d3a5f55fbef8cea73624
David Mendez <dmendez@ebi.ac.uk>
2016-02-29 16:03:21 +0000

    Organised the base layout in a template file

--- glados/urls.py
+++ glados/urls.py
@@ -4,4 +4,5 @@ from glados.utils import DirectTemplateView
 
 urlpatterns = [
   url(r'^compound_report_card/$', DirectTemplateView.as_view(template_name="glados/compoundReportCard.html"),),
+  url(r'^layout_test/$', DirectTemplateView.as_view(template_name="glados/layoutTest.html"),),
 ]
