5ed4431c44c64ae91559a7287759e0fd1d22f8cb
David Mendez <dmendez@ebi.ac.uk>
2016-03-14 15:47:53 +0000

    Reorganised functional tests to be in the same order as in backbone

--- glados/functional_tests.py
+++ glados/functional_tests.py
@@ -12,6 +12,25 @@ class CompoundReportCardTest(unittest.TestCase):
   def tearDown(self):
     self.browser.quit()
 
+  def test_compound_image(self):
+
+    # Normal structure image
+    self.browser.get('http://127.0.0.1:8000/compound_report_card/CHEMBL25')
+    img = self.browser.find_element_by_id('Bck-COMP_IMG')
+    self.assertEqual(img.get_attribute('src'), 'https://www.ebi.ac.uk/chembl/api/data/image/CHEMBL25')
+
+    # structure not available
+    self.browser.get('http://127.0.0.1:8000/compound_report_card/CHEMBL6963')
+    self.browser.implicitly_wait(self.IMPLICIT_WAIT)
+    img = self.browser.find_element_by_id('Bck-COMP_IMG')
+    self.assertEqual(img.get_attribute('src'), 'http://127.0.0.1:8000/static/img/structure_not_available.png')
+
+    # protein sctructure
+    self.browser.get('http://127.0.0.1:8000/compound_report_card/CHEMBL2108680')
+    self.browser.implicitly_wait(self.IMPLICIT_WAIT)
+    img = self.browser.find_element_by_id('Bck-COMP_IMG')
+    self.assertEqual(img.get_attribute('src'), 'http://127.0.0.1:8000/static/img/protein_structure.png')
+
   def test_compound_name(self):
 
     self.browser.get('http://127.0.0.1:8000/compound_report_card/CHEMBL25')
@@ -53,25 +72,6 @@ class CompoundReportCardTest(unittest.TestCase):
     self.browser.implicitly_wait(self.IMPLICIT_WAIT)
     phase_td = self.browser.find_element_by_id('Bck-MAX_PHASE')
     self.assertEqual('0', phase_td.text)
-
-  def test_compound_image(self):
-
-    # Normal structure image
-    self.browser.get('http://127.0.0.1:8000/compound_report_card/CHEMBL25')
-    img = self.browser.find_element_by_id('Bck-COMP_IMG')
-    self.assertEqual(img.get_attribute('src'), 'https://www.ebi.ac.uk/chembl/api/data/image/CHEMBL25')
-
-    # structure not available
-    self.browser.get('http://127.0.0.1:8000/compound_report_card/CHEMBL6963')
-    self.browser.implicitly_wait(self.IMPLICIT_WAIT)
-    img = self.browser.find_element_by_id('Bck-COMP_IMG')
-    self.assertEqual(img.get_attribute('src'), 'http://127.0.0.1:8000/static/img/structure_not_available.png')
-
-    # protein sctructure
-    self.browser.get('http://127.0.0.1:8000/compound_report_card/CHEMBL2108680')
-    self.browser.implicitly_wait(self.IMPLICIT_WAIT)
-    img = self.browser.find_element_by_id('Bck-COMP_IMG')
-    self.assertEqual(img.get_attribute('src'), 'http://127.0.0.1:8000/static/img/protein_structure.png')
 
 
 
