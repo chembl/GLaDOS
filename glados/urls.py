a58976ca16567a5d9e0f0a5be9634bde07fa2e6e
David Mendez <dmendez@ebi.ac.uk>
2016-03-16 11:33:36 +0000

    Allow to view CNC using <chembl_id>/embed/name_and_classification/

--- glados/urls.py
+++ glados/urls.py
@@ -4,5 +4,6 @@ from glados.utils import DirectTemplateView
 
 urlpatterns = [
   url(r'^compound_report_card/(?P<chembl_id>\w+)/$', DirectTemplateView.as_view(template_name="glados/compoundReportCard.html"),),
+  url(r'^compound_report_card/(?P<chembl_id>\w+)/embed/name_and_classification/$', DirectTemplateView.as_view(template_name="glados/CompoundReportCardParts/NameAndClassificationToEmbed.html"),),
   url(r'^layout_test/$', DirectTemplateView.as_view(template_name="glados/layoutTest.html"),),
 ]
