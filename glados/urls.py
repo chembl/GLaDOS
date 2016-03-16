a334e7c1a7e99d68df6c441b2da7eca1b8d776ac
David Mendez <dmendez@ebi.ac.uk>
2016-03-16 13:50:08 +0000

    Allow xframe_options_exempt when loading the embeded CNC.

--- glados/urls.py
+++ glados/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
-
 from glados.utils import DirectTemplateView
+from django.views.decorators.clickjacking import xframe_options_exempt
 
 urlpatterns = [
-  url(r'^compound_report_card/(?P<chembl_id>\w+)/$', DirectTemplateView.as_view(template_name="glados/compoundReportCard.html"),),
-  url(r'^compound_report_card/(?P<chembl_id>\w+)/embed/name_and_classification/$', DirectTemplateView.as_view(template_name="glados/CompoundReportCardParts/NameAndClassificationToEmbed.html"),),
-  url(r'^layout_test/$', DirectTemplateView.as_view(template_name="glados/layoutTest.html"),),
+  url(r'^compound_report_card/(?P<chembl_id>\w+)/$',
+      DirectTemplateView.as_view(template_name="glados/compoundReportCard.html"), ),
+  url(r'^compound_report_card/(?P<chembl_id>\w+)/embed/name_and_classification/$', xframe_options_exempt(
+    DirectTemplateView.as_view(template_name="glados/CompoundReportCardParts/NameAndClassificationToEmbed.html")), ),
+  url(r'^layout_test/$', DirectTemplateView.as_view(template_name="glados/layoutTest.html"), ),
 ]
