01b87fad3fee25501fda04baf40158aa42ceaba7
David Mendez <dmendez@ebi.ac.uk>
2016-03-30 16:41:30 +0100

    Remove console.logs, issue was identified, it comes from web services

--- glados/static/coffee/models/MechanismOfActionList.coffee
+++ glados/static/coffee/models/MechanismOfActionList.coffee
@@ -1,6 +1,4 @@
 MechanismOfActionList = Backbone.Collection.extend
   model: MechanismOfAction
   parse: (response) ->
-    console.log('getting mechanisms of action:')
-    console.log(response)
     return response.mechanisms;
--- glados/static/js/models/MechanismOfActionList.js
+++ glados/static/js/models/MechanismOfActionList.js
@@ -4,8 +4,6 @@ var MechanismOfActionList;
 MechanismOfActionList = Backbone.Collection.extend({
   model: MechanismOfAction,
   parse: function(response) {
-    console.log('getting mechanisms of action:');
-    console.log(response);
     return response.mechanisms;
   }
 });
