905dbb38f2bda0ac6b54e58a99d2c7f859aea6ee
David Mendez <dmendez@ebi.ac.uk>
2016-06-10 11:14:12 +0100

    rename template with confusing name.

--- glados/urls.py
+++ glados/urls.py
@@ -44,7 +44,7 @@ urlpatterns = [
   url(r'^$', views.main_page, name='main' ),
 
   url(r'^marvin_search_fullscreen/$',
-      DirectTemplateView.as_view(template_name="glados/marvin_search_small.html"), ),
+      DirectTemplateView.as_view(template_name="glados/marvin_search_fullscreen.html"), ),
 
   url(r'^layout_test/$', DirectTemplateView.as_view(template_name="glados/layoutTest.html"), ),
 
