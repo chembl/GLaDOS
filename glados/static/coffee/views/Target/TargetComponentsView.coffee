5e57d6a28c032830f128a59406ed84347b0dc4a2
David Mendez <dmendez@ebi.ac.uk>
2016-06-29 10:54:48 +0100

    Targ Rep Card: Load Components with what is currently available

--- glados/static/coffee/views/Target/TargetComponentsView.coffee
+++ glados/static/coffee/views/Target/TargetComponentsView.coffee
@@ -9,9 +9,18 @@ TargetComponentsView = CardView.extend
 
   render: ->
     @render_for_large()
+    @render_for_small()
 
   render_for_large: ->
 
     table_large = $(@el).find('#BCK-Components-large')
     template = $('#' + table_large.attr('data-hb-template'))
     table_large.html Handlebars.compile(template.html())
+      components: @model.get('target_components')
+
+  render_for_small: ->
+
+    table_large = $(@el).find('#BCK-Components-small')
+    template = $('#' + table_large.attr('data-hb-template'))
+    table_large.html Handlebars.compile(template.html())
+      components: @model.get('target_components')
--- glados/static/js/views/Target/TargetComponentsView.js
+++ glados/static/js/views/Target/TargetComponentsView.js
@@ -6,12 +6,23 @@ TargetComponentsView = CardView.extend({
     return this.model.on('change', this.render, this);
   },
   render: function() {
-    return this.render_for_large();
+    this.render_for_large();
+    return this.render_for_small();
   },
   render_for_large: function() {
     var table_large, template;
     table_large = $(this.el).find('#BCK-Components-large');
     template = $('#' + table_large.attr('data-hb-template'));
-    return table_large.html(Handlebars.compile(template.html()));
+    return table_large.html(Handlebars.compile(template.html())({
+      components: this.model.get('target_components')
+    }));
+  },
+  render_for_small: function() {
+    var table_large, template;
+    table_large = $(this.el).find('#BCK-Components-small');
+    template = $('#' + table_large.attr('data-hb-template'));
+    return table_large.html(Handlebars.compile(template.html())({
+      components: this.model.get('target_components')
+    }));
   }
 });
