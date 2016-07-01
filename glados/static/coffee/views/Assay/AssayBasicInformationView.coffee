541a689968dca1140a3751e6714368361c4758c5
David Mendez <dmendez@ebi.ac.uk>
2016-07-01 11:10:43 +0100

    Assay Report Card: make Basic-Information section live.

--- glados/static/coffee/views/Assay/AssayBasicInformationView.coffee
+++ glados/static/coffee/views/Assay/AssayBasicInformationView.coffee
@@ -9,4 +9,19 @@ AssayBasicInformationView = CardView.extend
 
   render: ->
 
-    console.log('render!')
+    @render_for_large()
+
+  render_for_large: ->
+
+    table_large = $(@el).find('#BCK-ABI-large')
+    template = $('#' + table_large.attr('data-hb-template'))
+
+    table_large.html Handlebars.compile(template.html())
+      chembl_id: @model.get('assay_chembl_id')
+      type: @model.get('assay_type_description')
+      description: @model.get('description')
+      format: @model.get('bao_format')
+      organism: @model.get('assay_organism')
+      strain: @model.get('assay_strain')
+      cell_type: @model.get('cell_type')
+      subcellular_fraction: @model.get('assay_subcellular_fraction')
--- glados/static/js/views/Assay/AssayBasicInformationView.js
+++ glados/static/js/views/Assay/AssayBasicInformationView.js
@@ -6,6 +6,21 @@ AssayBasicInformationView = CardView.extend({
     return this.model.on('change', this.render, this);
   },
   render: function() {
-    return console.log('render!');
+    return this.render_for_large();
+  },
+  render_for_large: function() {
+    var table_large, template;
+    table_large = $(this.el).find('#BCK-ABI-large');
+    template = $('#' + table_large.attr('data-hb-template'));
+    return table_large.html(Handlebars.compile(template.html())({
+      chembl_id: this.model.get('assay_chembl_id'),
+      type: this.model.get('assay_type_description'),
+      description: this.model.get('description'),
+      format: this.model.get('bao_format'),
+      organism: this.model.get('assay_organism'),
+      strain: this.model.get('assay_strain'),
+      cell_type: this.model.get('cell_type'),
+      subcellular_fraction: this.model.get('assay_subcellular_fraction')
+    }));
   }
 });
