d43a07304b1bf98e9a28c622a84dc4a7de7b128f
David Mendez <dmendez@ebi.ac.uk>
2016-07-21 14:47:43 +0100

    Target Browser: Get target classification information.

--- glados/static/coffee/models/Target.coffee
+++ glados/static/coffee/models/Target.coffee
@@ -3,5 +3,55 @@ Target = Backbone.RelationalModel.extend
   idAttribute: 'target_chembl_id'
 
   initialize: ->
+    @on 'change', @getProteinTargetClassification, @
     @url = 'https://www.ebi.ac.uk/chembl/api/data/target/' + @get('target_chembl_id') + '.json'
 
+  getProteinTargetClassification: ->
+
+    target_components = @get('target_components')
+    console.log('changed!')
+    console.log(@)
+
+    if target_components?
+
+      @set('protein_classifications', {}, {silent:true})
+      console.log('getProteinTargetClassification()!')
+
+      get_proxy = $.proxy(@get, @)
+
+      # step 1: I get the component id of each of the target components
+      for comp in target_components
+        component_id = comp['component_id']
+        comp_url = 'https://www.ebi.ac.uk/chembl/api/data/target_component/' + component_id + '.json'
+
+        # step 2: I request the details of the current component
+        $.get(comp_url).done(
+
+          (data) ->
+
+            protein_classifications = data['protein_classifications']
+            for prot_class in protein_classifications
+
+              prot_class_id = prot_class['protein_classification_id']
+
+              # step 3: Now that I got the protein class id, I need to check if it is already there, it can be repeated
+              # so I will only make a call when necessary
+              prot_class_dict = get_proxy('protein_classifications')
+
+              if !prot_class_dict[prot_class_id]?
+                # I don't have that protein classification, So I need to add it to the dictionary and make a
+                # call to get the details
+                prot_class_dict[prot_class_id] = []
+                prot_class_url = 'https://www.ebi.ac.uk/chembl/api/data/protein_class/' + prot_class_id + '.json'
+
+                $.get(prot_class_url).done(
+                  (data) ->
+                     prot_class_dict[data['protein_class_id']] = ( data['l' + num] for num in [1..8] )
+                ).fail(
+                  () -> console.log('failed2!')
+                )
+
+
+        ).fail(
+          () -> console.log('failed!')
+        )
--- glados/static/js/models/Target.js
+++ glados/static/js/models/Target.js
@@ -4,6 +4,59 @@ var Target;
 Target = Backbone.RelationalModel.extend({
   idAttribute: 'target_chembl_id',
   initialize: function() {
+    this.on('change', this.getProteinTargetClassification, this);
     return this.url = 'https://www.ebi.ac.uk/chembl/api/data/target/' + this.get('target_chembl_id') + '.json';
+  },
+  getProteinTargetClassification: function() {
+    var comp, comp_url, component_id, get_proxy, target_components, _i, _len, _results;
+    target_components = this.get('target_components');
+    console.log('changed!');
+    console.log(this);
+    if (target_components != null) {
+      this.set('protein_classifications', {}, {
+        silent: true
+      });
+      console.log('getProteinTargetClassification()!');
+      get_proxy = $.proxy(this.get, this);
+      _results = [];
+      for (_i = 0, _len = target_components.length; _i < _len; _i++) {
+        comp = target_components[_i];
+        component_id = comp['component_id'];
+        comp_url = 'https://www.ebi.ac.uk/chembl/api/data/target_component/' + component_id + '.json';
+        _results.push($.get(comp_url).done(function(data) {
+          var prot_class, prot_class_dict, prot_class_id, prot_class_url, protein_classifications, _j, _len1, _results1;
+          protein_classifications = data['protein_classifications'];
+          _results1 = [];
+          for (_j = 0, _len1 = protein_classifications.length; _j < _len1; _j++) {
+            prot_class = protein_classifications[_j];
+            prot_class_id = prot_class['protein_classification_id'];
+            prot_class_dict = get_proxy('protein_classifications');
+            if (!(prot_class_dict[prot_class_id] != null)) {
+              prot_class_dict[prot_class_id] = [];
+              prot_class_url = 'https://www.ebi.ac.uk/chembl/api/data/protein_class/' + prot_class_id + '.json';
+              _results1.push($.get(prot_class_url).done(function(data) {
+                var num;
+                return prot_class_dict[data['protein_class_id']] = (function() {
+                  var _k, _results2;
+                  _results2 = [];
+                  for (num = _k = 1; _k <= 8; num = ++_k) {
+                    _results2.push(data['l' + num]);
+                  }
+                  return _results2;
+                })();
+              }).fail(function() {
+                return console.log('failed2!');
+              }));
+            } else {
+              _results1.push(void 0);
+            }
+          }
+          return _results1;
+        }).fail(function() {
+          return console.log('failed!');
+        }));
+      }
+      return _results;
+    }
   }
 });
