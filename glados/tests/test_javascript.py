f827dca08ea95fb5c21b34aa904bf9059119af51
Juan F. Mosquera <juanfmx2@gmail.com>
2016-10-28 11:06:49 +0100

    Changes to javascript slenium tester to include failure information

--- glados/tests/test_javascript.py
+++ glados/tests/test_javascript.py
@@ -7,17 +7,29 @@ class JavascriptTest(ReportCardTester):
 
   def test_javascript_code_only(self):
 
-    self.getURL(self.HOST + '/js_tests/', ReportCardTester.SLEEP_TIME * 10 )
+    self.getURL(self.HOST + '/js_tests/', ReportCardTester.SLEEP_TIME * 10)
+    tests_summary = None
     try:
-      tests_summary = self.browser.find_element_by_class_name('jasmine-alert').find_element_by_class_name('jasmine-failed')
+      tests_summary = self.browser.find_element_by_class_name('jasmine-alert')\
+        .find_element_by_class_name('jasmine-failed')
     except:
       pass
     # Fails if the jasmine tests failed
     if tests_summary and tests_summary.text:
-      self.fail(tests_summary.text)
+      failures_parent = self.browser.find_element_by_class_name('jasmine-failures')
+      fails = failures_parent.find_elements_by_class_name('jasmine-failed')
+      errors = ''
+      for fail_i in fails:
+        desc = self.browser.find_element_by_class_name('jasmine-description').find_element_by_tag_name('a').text
+        msg = self.browser.find_element_by_class_name('jasmine-result-message').text
+        stack = self.browser.find_element_by_class_name('jasmine-stack-trace').text
+        errors += "ERROR_DESC:{0}\nERROR_MSG:{1}\nERROR_STACK:\n{2}\n".format(desc, msg, stack)
+
+      self.fail("Jasmine test failed:\n{0}\n{1}".format(tests_summary.text, errors))
 
     try:
-      tests_summary = self.browser.find_element_by_class_name('jasmine-alert').find_element_by_class_name('jasmine-passed')
+      tests_summary = self.browser.find_element_by_class_name('jasmine-alert')\
+        .find_element_by_class_name('jasmine-passed')
     except:
       self.fail("Could not find jasmine passed output!")
 
