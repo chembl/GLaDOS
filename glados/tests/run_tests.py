9bf0a338ed609db9102a34e54fb6f128dca58578
Juan F. Mosquera <juanfmx2@gmail.com>
2016-11-24 23:50:12 +0000

    Fix for test runner

--- glados/tests/run_tests.py
+++ glados/tests/run_tests.py
@@ -1,9 +1,9 @@
 import os
 
-if __name__ == "__main__" or __name__ == "glados.run_tests":
-  os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
+if __name__ == "__main__" or __name__ == "glados.tests.run_tests":
+  os.environ['DJANGO_SETTINGS_MODULE'] = 'glados.settings'
 
   print('starting tests!!')
   from django.core.management import execute_from_command_line
   # 3rd argument is the directory for where to look for tests
-  execute_from_command_line(['runtests.py', 'test', 'glados.tests'])
+  execute_from_command_line(['test', 'glados.tests'])
