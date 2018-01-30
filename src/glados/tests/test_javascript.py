42b16fd0dd2583b0fe8ecef1f2ff495a7a9e6371
Juan F. Mosquera <jfmosquera@ebi.ac.uk>
2018-01-30 16:43:32 +0000

    Fix Tests: jasmine debug

--- src/glados/tests/test_javascript.py
+++ src/glados/tests/test_javascript.py
@@ -13,6 +13,7 @@ class JavascriptTest(ReportCardTester):
   def test_javascript_code_only(self):
     url = self.HOST + '/js_tests/'
     self.getURL(url, wait_for_glados_ready=False)
+    time.sleep(5)
     print('-----HTML-------------------------------------------')
     print(self.browser.find_element_by_css_selector('html').get_attribute('innerHTML'))
     print('-----HTML-------------------------------------------\n')
