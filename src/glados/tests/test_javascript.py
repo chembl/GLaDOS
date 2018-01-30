892d300e6c1d7e64b1e532f976130673cd5c9a41
Juan F. Mosquera <jfmosquera@ebi.ac.uk>
2018-01-30 15:24:41 +0000

    Fix Tests: update to fix multiple calls to the same element

--- src/glados/tests/test_javascript.py
+++ src/glados/tests/test_javascript.py
@@ -13,33 +13,35 @@ class JavascriptTest(ReportCardTester):
   def test_javascript_code_only(self):
     url = self.HOST + '/js_tests/'
     self.getURL(url, wait_for_glados_ready=False)
-    tests_summary = None
     timeout = ReportCardTester.DEFAULT_TIMEOUT*2
     loaded = False
     start_time = time.time()
+    jasmine_alert_elem = None
+    jasmine_failed_elem = None
+    jasmine_passed_elem = None
     while not loaded and time.time() - start_time < timeout:
-      try:
-        elem = self.browser.find_element_by_class_name('jasmine-alert')\
-          .find_element_by_class_name('jasmine-failed')
-        loaded = True
-      except:
-        pass
-      try:
-        elem = self.browser.find_element_by_class_name('jasmine-alert')\
-          .find_element_by_class_name('jasmine-passed')
-        loaded = True
-      except:
-        pass
-      print("Loading {0} ...".format(url))
-      time.sleep(1)
+      if jasmine_alert_elem is None:
+        try:
+          jasmine_alert_elem = self.browser.find_element_by_class_name('jasmine-alert')
+        except:
+          pass
+      else:
+        try:
+          jasmine_failed_elem = jasmine_alert_elem.find_element_by_class_name('jasmine-failed')
+          loaded = True
+        except:
+          pass
+        try:
+          jasmine_passed_elem = jasmine_alert_elem.find_element_by_class_name('jasmine-passed')
+          loaded = True
+        except:
+          pass
+        if not loaded:
+          print("Loading {0} ...".format(url))
+          time.sleep(1)
     self.assertTrue(loaded, "Error: '{0}' failed to load under {1} seconds!".format(url, timeout))
-    try:
-      tests_summary = self.browser.find_element_by_class_name('jasmine-alert')\
-        .find_element_by_class_name('jasmine-failed')
-    except:
-      pass
-    # Fails if the jasmine tests failed
-    if tests_summary and tests_summary.text:
+
+    if jasmine_failed_elem is not None and jasmine_failed_elem.text:
       failures_parent = self.browser.find_element_by_class_name('jasmine-failures')
       fails = failures_parent.find_elements_by_class_name('jasmine-failed')
       errors = ''
@@ -49,17 +51,13 @@ class JavascriptTest(ReportCardTester):
         stack = self.browser.find_element_by_class_name('jasmine-stack-trace').text
         errors += "ERROR_DESC:{0}\nERROR_MSG:{1}\nERROR_STACK:\n{2}\n".format(desc, msg, stack)
 
-      self.fail("Jasmine test failed:\n{0}\n{1}".format(tests_summary.text, errors))
+      self.fail("Jasmine test failed:\n{0}\n{1}".format(jasmine_failed_elem.text, errors))
 
-    try:
-      tests_summary = self.browser.find_element_by_class_name('jasmine-alert')\
-        .find_element_by_class_name('jasmine-passed')
-    except:
+    if jasmine_passed_elem is None:
       self.fail("Could not find jasmine passed output!")
-
-    summary_text = tests_summary.text
-
-    self.assertIn(', 0 failures', summary_text, 'Check the javascript tests out! ' + self.HOST + '/js_tests/' )
+    else:
+      summary_text = jasmine_passed_elem.text
+      self.assertIn(', 0 failures', summary_text, 'Check the javascript tests out! ' + self.HOST + '/js_tests/' )
 
 
 
