84b2adcbdcc6cce3cd6b7d0a3acf1861375ba2f4
Juan F. Mosquera <jfmosquera@ebi.ac.uk>
2018-02-19 12:50:46 +0000

    log IS SECURE django details

--- src/glados/settings_context.py
+++ src/glados/settings_context.py
@@ -3,6 +3,7 @@ from django.conf import settings
 
 # Additional variables required for the templates
 def glados_settings_context_processor(request):
+    print('IS SECURE:', request.is_secure(), request.scheme)
     gsc_vars = {
         'request_root_url': request.build_absolute_uri('/'),
         'js_debug': 'true' if settings.DEBUG else 'false'
