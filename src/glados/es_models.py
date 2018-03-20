8e811d83d9a098d787554ac3c5166d3e11db2dea
Juan F. Mosquera <jfmosquera@ebi.ac.uk>
2018-03-20 18:29:09 +0000

    Bulk Queries from server: comparison fix

--- src/glados/es_models.py
+++ src/glados/es_models.py
@@ -5,11 +5,11 @@ from elasticsearch import Elasticsearch
 from . import models
 from django.conf import settings
 
-if settings.ELASTICSEARCH_PASSWORD == None:
+if settings.ELASTICSEARCH_PASSWORD is None:
   connections.create_connection(hosts=[settings.ELASTICSEARCH_HOST])
 else:
   connections.create_connection(hosts=[settings.ELASTICSEARCH_HOST],
-                              http_auth=(settings.ELASTICSEARCH_USERNAME, settings.ELASTICSEARCH_PASSWORD))
+                                http_auth=(settings.ELASTICSEARCH_USERNAME, settings.ELASTICSEARCH_PASSWORD))
 
 class TinyURLIndex(DocType):
   long_url = Text()
