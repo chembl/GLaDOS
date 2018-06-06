f12dfc8b22c703800b27652b878bc2efadf8ec3b
Juan F. Mosquera <jfmosquera@ebi.ac.uk>
2018-06-06 16:13:03 +0100

    fix for fasta to ignore queries with less than 10 characters

--- src/glados/grammar/fasta.py
+++ src/glados/grammar/fasta.py
@@ -1,10 +1,17 @@
-from arpeggio import Optional, OneOrMore, ZeroOrMore, Not
+from arpeggio import Optional, OneOrMore, ZeroOrMore, Not, Repetition
 from arpeggio import RegExMatch as _
 import glados.grammar.common as common
 
 
+def sequence_part():
+    return _(r'[A-Z-*]')
+
+
 def protein_sequence():
-    return OneOrMore(_(r'[A-Z-*]'))
+    min_ten = (sequence_part,)*10
+    continuation = (OneOrMore(sequence_part), )
+    complete_tuple = min_ten + continuation
+    return complete_tuple
 
 
 def sequence_id():
@@ -13,4 +20,3 @@ def sequence_id():
 
 def fasta():
     return Optional(sequence_id), protein_sequence(), common.term_end_lookahead
-
