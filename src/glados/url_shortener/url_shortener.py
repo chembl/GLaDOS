cdca4a2df745c75c650412ba6002124cf37e1c5e
Juan F. Mosquera <jfmosquera@ebi.ac.uk>
2018-08-13 23:23:12 +0100

    Minor: verb tense correction

--- src/glados/url_shortener/url_shortener.py
+++ src/glados/url_shortener/url_shortener.py
@@ -14,7 +14,7 @@ def shorten_url(long_url):
   if response.hits.total == 0:
     tinyURL = TinyURL(long_url=long_url, hash=hash)
     tinyURL.indexing()
-    print('has not been save before!')
+    print('has not been saved before!')
 
   return hash
 
