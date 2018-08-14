03588587bc5adb7c0963286280f095272d6ee352
Juan F. Mosquera <jfmosquera@ebi.ac.uk>
2018-08-14 15:20:00 +0100

    Logging: proper logging usage for pre server start functionalities

--- src/glados/manage.py
+++ src/glados/manage.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import logging.config
 
 
 def main():
@@ -7,12 +8,14 @@ def main():
 
     from django.core.management import execute_from_command_line
     from django.conf import settings
+
+    logging.config.dictConfig(settings.LOGGING)
+
     import glados.static_files_compiler
     # Compress files before server launch if compression is enabled
     if os.environ.get('RUN_MAIN') != 'true' and len(sys.argv) > 1 and sys.argv[1] == 'runserver' and settings.DEBUG:
         glados.static_files_compiler.StaticFilesCompiler.compile_coffee()
         glados.static_files_compiler.StaticFilesCompiler.compile_scss()
-        glados.static_files_compiler.enable_debug_logging()
         execute_from_command_line([sys.argv[0], 'compilemessages'])
     elif os.environ.get('RUN_MAIN') != 'true' and len(sys.argv) > 1 and sys.argv[1] == 'collectstatic':
         glados.static_files_compiler.StaticFilesCompiler.compile_coffee()
