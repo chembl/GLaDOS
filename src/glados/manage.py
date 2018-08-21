77065f253002681936378c78a080f680b37c7316
Juan F. Mosquera <jfmosquera@ebi.ac.uk>
2018-08-21 11:24:22 +0100

    StaticFiles Compiler: use of new joined method to compile all

--- src/glados/manage.py
+++ src/glados/manage.py
@@ -14,12 +14,10 @@ def main():
     import glados.static_files_compiler
     # Compress files before server launch if compression is enabled
     if os.environ.get('RUN_MAIN') != 'true' and len(sys.argv) > 1 and sys.argv[1] == 'runserver' and settings.DEBUG:
-        glados.static_files_compiler.StaticFilesCompiler.compile_coffee()
-        glados.static_files_compiler.StaticFilesCompiler.compile_scss()
+        glados.static_files_compiler.StaticFilesCompiler.compile_all_known_compilers()
         execute_from_command_line([sys.argv[0], 'compilemessages'])
     elif os.environ.get('RUN_MAIN') != 'true' and len(sys.argv) > 1 and sys.argv[1] == 'collectstatic':
-        glados.static_files_compiler.StaticFilesCompiler.compile_coffee()
-        glados.static_files_compiler.StaticFilesCompiler.compile_scss()
+        glados.static_files_compiler.StaticFilesCompiler.compile_all_known_compilers()
         execute_from_command_line([sys.argv[0], 'compilemessages', '--settings=glados'])
         if settings.COMPRESS_ENABLED and settings.COMPRESS_OFFLINE:
             execute_from_command_line([sys.argv[0], 'compress'])
