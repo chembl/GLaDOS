778eb74b5539406071fea40a05d330e5681a8080
Ric <ricartomojo@gmail.com>
2018-08-31 13:40:10 +0100

    fixed static files path for unicheapp

--- src/glados/unichem/config/index.js
+++ src/glados/unichem/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../../static'),
     assetsSubDirectory: 'unichem_gen',
-    assetsPublicPath: '/static/',
+    assetsPublicPath: 'static/',
 
     /**
      * Source Maps
