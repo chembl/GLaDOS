ade09a92c0338201aeee3638db17e719872191d2
David Mendez <dmendez@ebi.ac.uk>
2018-11-15 13:28:22 +0000

    Markup: do not attempt to generate it when compound does not exist

--- src/glados/schema_tags_generator.py
+++ src/glados/schema_tags_generator.py
@@ -3,7 +3,28 @@ from elasticsearch_dsl import Search
 
 def get_schema_obj_for_compound(chembl_id):
 
+    q = {
+        "query_string": {
+            "default_field": "molecule_chembl_id",
+            "query": chembl_id
+        }
+    }
+    s = Search(index="chembl_molecule").query(q)
+    response = s.execute()
+    dont_generate_metadata = response.hits.total == 0
+
+    if dont_generate_metadata:
+        schema_obj = {
+            'metadata_generated': False
+        }
+        return schema_obj
+
+    molecule_type = response['hits']
+    print('molecule_type: ', molecule_type)
+
+
     schema_obj = {
+        'metadata_generated': True,
         'identifier': chembl_id
     }
     return schema_obj
--- src/glados/tests/test_metadata_generation.py
+++ src/glados/tests/test_metadata_generation.py
@@ -1,17 +1,25 @@
 import unittest
+import os
+from glados import es_connection
 from glados import schema_tags_generator
 
 class MetadataGenerationTester(unittest.TestCase):
 
 
     def setUp(self):
+        os.environ['DJANGO_SETTINGS_MODULE'] = 'glados.settings'
+        es_connection.setup_glados_es_connection()
         print('Running Test: {0}'.format(self._testMethodName))
+
+    def test_metadata_is_not_produced_for_nonexistent_compounds(self):
+
+        schema_obj_got = schema_tags_generator.get_schema_obj_for_compound('nonexistent')
+        self.assertFalse(schema_obj_got['metadata_generated'])
 
     def test_metadata_is_not_produced_for_some_compound_types(self):
 
-        pass
         # # not generated for compounds type unclassified
-        # schema_obj_got = schema_tags_generator.get_schema_obj_for_compound('CHEMBL2108378')
-        # print('schema_obj_got: ')
-        # print(schema_obj_got)
-        # pass
+        schema_obj_got = schema_tags_generator.get_schema_obj_for_compound('CHEMBL2108378')
+        print('schema_obj_got: ')
+        print(schema_obj_got)
+        pass
