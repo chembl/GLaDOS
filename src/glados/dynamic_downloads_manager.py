c8140fd330633f63cab7afa66e2130bab4719994
David Mendez <dmendez@ebi.ac.uk>
2018-11-29 11:39:34 +0000

    Use eralier versions of redis and rq to avoid a known issue

--- src/glados/dynamic_downloads_manager.py
+++ src/glados/dynamic_downloads_manager.py
@@ -1,7 +1,18 @@
 # this handles the creation of dynamically generated downloads
+import time
+from django_rq import job
+from django_rq import get_worker
+
+@job
+def generate_download_file():
+
+    num = 10
+    for i in range(num):
+        print('i: ', i)
+        time.sleep(1)
 
 
 def generate_download():
-
+    generate_download_file.delay()
     response = {}
     return response
