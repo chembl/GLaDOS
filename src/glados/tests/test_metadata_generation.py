46937ffa665182ab28a0d247a874df8fa84a26f7
David Mendez <dmendez@ebi.ac.uk>
2018-12-07 14:28:34 +0000

    Travis: do not overwrite the elasticsearch host in metadata tests

--- src/glados/tests/test_metadata_generation.py
+++ src/glados/tests/test_metadata_generation.py
@@ -9,7 +9,6 @@ class MetadataGenerationTester(unittest.TestCase):
 
     def setUp(self):
         os.environ['DJANGO_SETTINGS_MODULE'] = 'glados.settings'
-        settings.ELASTICSEARCH_HOST = 'https://www.ebi.ac.uk/chembl/glados-es/'
         es_connection.setup_glados_es_connection()
         print('Running Test: {0}'.format(self._testMethodName))
 
