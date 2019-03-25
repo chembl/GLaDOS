e09a891b9aa92012c75dd055a262af0e0adfd4bc
David Mendez <dmendez@ebi.ac.uk>
2019-03-25 09:47:45 +0000

    URL Shortener: fix indentation

--- src/glados/api/url_shortening/url_shortener.py
+++ src/glados/api/url_shortening/url_shortener.py
@@ -30,34 +30,32 @@ def process_extend_url_request(request, hash):
 
 # given a long url, it shortens it and saves it in elastic, it returns the hash obtained
 def shorten_url(long_url):
+    hex_digest = hashlib.md5(long_url.encode('utf-8')).digest()
+    # replace / and + to avoid routing problems
+    hash = base64.b64encode(hex_digest).decode('utf-8').replace('/', '_').replace('+', '-')
 
-  hex_digest = hashlib.md5(long_url.encode('utf-8')).digest()
-  # replace / and + to avoid routing problems
-  hash = base64.b64encode(hex_digest).decode('utf-8').replace('/', '_').replace('+', '-')
+    # save this in elastic if it doesn't exist
+    s = Search().filter('query_string', query='"' + hash + '"')
+    response = s.execute()
+    if response.hits.total == 0:
+        dt = datetime.now()
+        td = timedelta(days=4)
+        expiration_date = dt + td
+        expires = expiration_date.timestamp() * 1000
 
-  # save this in elastic if it doesn't exist
-  s = Search().filter('query_string', query='"' + hash + '"')
-  response = s.execute()
-  if response.hits.total == 0:
+        tinyURL = TinyURL(long_url=long_url, hash=hash, expires=expires)
+        tinyURL.indexing()
+        print('has not been saved before!')
 
-    dt = datetime.now()
-    td = timedelta(days=4)
-    expiration_date = dt + td
-    expires = expiration_date.timestamp() * 1000
+    return hash
 
-    tinyURL = TinyURL(long_url=long_url, hash=hash, expires=expires)
-    tinyURL.indexing()
-    print('has not been saved before!')
-
-  return hash
 
 def get_original_url(hash):
+    # look here in elastic
+    s = Search().filter('query_string', query='"' + hash + '"')
+    response = s.execute(ignore_cache=True)
+    if response.hits.total == 0:
+        return None
 
-  #look here in elastic
-  s = Search().filter('query_string', query='"' + hash + '"')
-  response = s.execute(ignore_cache=True)
-  if response.hits.total == 0:
-    return None
-
-  url = response.hits[0].long_url
-  return url
+    url = response.hits[0].long_url
+    return url
