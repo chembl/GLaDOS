c9374381bab6fd7d3db1018d03d29f4d5735bf59
David Mendez <dmendez@ebi.ac.uk>
2019-03-26 10:35:33 +0000

    Add link to old interface in header

--- src/glados/urls.py
+++ src/glados/urls.py
@@ -120,6 +120,10 @@ common_urls = [
         RedirectView.as_view(url='https://chembl.gitbook.io/chembl-interface-documentation/web-services',
                              permanent=True), name='web_services_home'),
 
+    url(r'^old_interface/$',
+        RedirectView.as_view(url=settings.OLD_INTERFACE_URL,
+                             permanent=False), name='old_interface_home'),
+
     url(r'^unichem_home/$',
         RedirectView.as_view(url='https://www.ebi.ac.uk/unichem',
                              permanent=True), name='unichem_home'),
