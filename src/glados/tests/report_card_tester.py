5d05cf0233c8e3feb3f59aca2ef329e5929506ea
David Mendez <dmendez@ebi.ac.uk>
2019-05-10 09:25:55 +0100

    remove unused code

--- src/glados/tests/report_card_tester.py
+++ src/glados/tests/report_card_tester.py
@@ -98,23 +98,3 @@ class ReportCardTester(unittest.TestCase):
             return
         time.sleep(1)
       self.assertTrue(loaded, "Error: '{0}' failed to load under {1} seconds!".format(url, timeout))
-
-  def assert_embed_trigger(self, card_id, resource_type, section_name, chembl_id):
-
-    card = self.browser.find_element_by_id(card_id)
-    embed_trigger = card.find_element_by_class_name('embed-btn')
-    self.assertEqual(embed_trigger.get_attribute('href'), '%s/%s_report_card/%s/#embed-modal-for-%s' % (self.HOST, resource_type, chembl_id, card_id) )
-    self.assertEqual(embed_trigger.get_attribute('data-embed-sect-name'), section_name)
-    self.assertEqual(embed_trigger.get_attribute('data-resource-type'), resource_type)
-
-  def assert_table(self, table, texts_should_be):
-
-    rows = table.find_elements(By.CSS_SELECTOR, "tbody tr")[1::]
-    for row, text in zip(rows, texts_should_be):
-
-      self.assertEqual(row.text, text)
-
-  def assert_table_any_order(self, table, texts_should_be):
-    rows = table.find_elements(By.TAG_NAME, " tbody tr")[1::]
-    for row in rows:
-      self.assertIn(row.text, texts_should_be)
