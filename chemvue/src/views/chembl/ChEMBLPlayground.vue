7408e3bebe4dedb06f38bdbdc4b3e7c4159266dd
David Mendez <dmendez@ebi.ac.uk>
2019-06-04 11:28:00 +0100

    Chemvue: start to add dialog as base for chemical skecther

--- chemvue/src/views/chembl/ChEMBLPlayground.vue
+++ chemvue/src/views/chembl/ChEMBLPlayground.vue
@@ -1,36 +1,61 @@
 <template>
   <v-container fluid class="container-story">
-    <v-slide-y-transition mode="out-in">
-      <h1>Hello World</h1>
-    </v-slide-y-transition>
+
+    <v-dialog
+      v-model="dialog"
+      width="500"
+    >
+      <template v-slot:activator="{ on }">
+        <v-btn
+          color="primary"
+          dark
+          v-on="on"
+        >
+          Click Me
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-card-title
+          class="headline grey lighten-2"
+          primary-title
+        >
+          Privacy Policy
+        </v-card-title>
+
+        <v-card-text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            dark color="primary"
+            flat
+            @click="dialog = false"
+          >
+            I accept
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
   </v-container>
 </template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1,
-h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 
-.container-story {
-  max-height: 100vh;
-  overflow: auto;
-}
 </style>
 
 <script>
-import Vue from "vue";
-export default {};
+export default {
+  data () {
+    return {
+      dialog: false
+    }
+  }
+}
 </script>
