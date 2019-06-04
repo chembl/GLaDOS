783dd9fd9d8b3cbc6db6b6c13287fdd18f950d16
David Mendez <dmendez@ebi.ac.uk>
2019-06-04 11:52:51 +0100

    Chemvue: create new component for structure search modal

---
+++ chemvue/src/components/shared/chembl/StructureSearchModal.vue
@@ -0,0 +1,62 @@
+<template>
+    <v-dialog
+      v-model="dialog"
+      width="500"
+    >
+      <template v-slot:activator="{ on }">
+        <v-btn
+          color="primary"
+          dark
+          v-on="on"
+        >
+          Click Me
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-card-title
+          class="headline grey lighten-2"
+          primary-title
+        >
+          Draw a Structure
+        </v-card-title>
+
+        <v-card-text>
+          Editor comes here!
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+
+          <v-btn dark color="primary" @click="dialog = false" >
+            Connectivity
+          </v-btn>
+          <v-spacer></v-spacer>
+
+          <v-btn dark color="primary" @click="dialog = false">
+            Similarity
+          </v-btn>
+          
+          <v-spacer></v-spacer>
+          <v-btn dark color="primary" @click="dialog = false">
+            Substructure
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      dialog: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- chemvue/src/views/chembl/ChEMBLPlayground.vue
+++ chemvue/src/views/chembl/ChEMBLPlayground.vue
@@ -1,46 +1,10 @@
 <template>
   <v-container fluid class="container-story">
 
-    <v-dialog
-      v-model="dialog"
-      width="500"
-    >
-      <template v-slot:activator="{ on }">
-        <v-btn
-          color="primary"
-          dark
-          v-on="on"
-        >
-          Click Me
-        </v-btn>
-      </template>
+    Structure search modal:
+    <br>
+    <StructureSearchModal/>
 
-      <v-card>
-        <v-card-title
-          class="headline grey lighten-2"
-          primary-title
-        >
-          Privacy Policy
-        </v-card-title>
-
-        <v-card-text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </v-card-text>
-
-        <v-divider></v-divider>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            dark color="primary"
-            flat
-            @click="dialog = false"
-          >
-            I accept
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
 
   </v-container>
 </template>
@@ -51,11 +15,10 @@
 </style>
 
 <script>
+import StructureSearchModal from '@/components/shared/chembl/StructureSearchModal.vue'
 export default {
-  data () {
-    return {
-      dialog: false
+    components: {
+      StructureSearchModal
     }
-  }
 }
 </script>
