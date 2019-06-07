ca2966164bf4ff9a42b0ffdd744a09c7763458f9
David Mendez <dmendez@ebi.ac.uk>
2019-06-07 15:02:49 +0100

    Properties Configuration: raise error when the index does not exist

--- src/glados/es_properties_configuration/configuration_getter.py
+++ src/glados/es_properties_configuration/configuration_getter.py
@@ -1,11 +1,24 @@
-import glados.ws2es.es_util as es_util
+import glados.ws2es.resources_description as resources_description
+
+
+class ESPropsConfigurationGetterError(Exception):
+    """Base class for exceptions in GLaDOS configuration."""
+    pass
 
 
 def get_config_for(index_name, prop_id):
 
-    index_mapping = es_util.get_index_mapping(index_name)
-
+    print('index_name: ', index_name)
+    print('RESOURCES_BY_ALIAS_NAME: ', resources_description.RESOURCES_BY_ALIAS_NAME)
+    index_mapping = resources_description.RESOURCES_BY_ALIAS_NAME.get(index_name)
     print('index_mapping: ', index_mapping)
+    if index_mapping is None:
+        raise ESPropsConfigurationGetterError("The index {} does not exist!".format(index_name))
+
+    # simplified_mapping = index_mapping.get_simplified_mapping_from_es()
+    # print('simplified_mapping: ', simplified_mapping)
+
+    # print('index_mapping: ', index_mapping)
     config = {
         'index_name': index_name,
         'prop_id': prop_id,
--- src/glados/es_properties_configuration/tests/tests.py
+++ src/glados/es_properties_configuration/tests/tests.py
@@ -1,8 +1,23 @@
 import unittest
 from glados.es_properties_configuration import configuration_getter
+import glados.ws2es.es_util as es_util
 
 
 class ConfigurationGetterTester(unittest.TestCase):
+
+    def setUp(self):
+        es_util.setup_connection('wp-p1m-50.ebi.ac.uk', 9200)
+
+    def test_fails_when_index_does_not_exist(self):
+
+        index_name = 'does_not_exist'
+        prop_id = '_metadata.assay_data.assay_subcellular_fraction'
+
+        try:
+            config_got = configuration_getter.get_config_for(index_name, prop_id)
+            self.assertTrue(False, 'This should have thrown an exception for a non existing index!')
+        except configuration_getter.ESPropsConfigurationGetterError:
+            pass
 
     def test_gets_config_for_one_property_with_no_override(self):
 
