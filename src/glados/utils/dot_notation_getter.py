4d7dcead13d46cd97d3bebfe6c333fce0127240e
David Mendez <dmendez@ebi.ac.uk>
2019-06-13 15:21:36 +0100

    Move DotNotationGetter class to utils module

---
+++ src/glados/utils/dot_notation_getter.py
@@ -0,0 +1,23 @@
+class DotNotationGetter:
+    DEFAULT_NULL_LABEL = ''
+
+    def __init__(self, obj):
+        self.obj = obj
+
+    def get_property(self, obj, str_property):
+        prop_parts = str_property.split('.')
+        current_prop = prop_parts[0]
+        if len(prop_parts) > 1:
+            current_obj = obj.get(current_prop)
+            if current_obj is None:
+                return self.DEFAULT_NULL_LABEL
+            else:
+                return self.get_property(current_obj, '.'.join(prop_parts[1::]))
+        else:
+
+            value = obj.get(current_prop)
+            value = self.DEFAULT_NULL_LABEL if value is None else value
+            return value
+
+    def get_from_string(self, dot_notation_property):
+        return self.get_property(self.obj, dot_notation_property)
---
+++ src/glados/utils/tests/test_dot_notation_getter.py
@@ -0,0 +1,47 @@
+from django.test import TestCase
+from glados.utils.dot_notation_getter import DotNotationGetter
+
+
+class DotNotationGetterTester(TestCase):
+    """This class allows to obtain proterties in a dictionary using a string with dots, such as
+    '_metadata.assay_data.assay_subcellular_fraction'"""
+
+    test_dict = {
+        'brand': 'Ford',
+        'model': 'Mustang',
+        'year': 1964,
+        'owner': {
+            'name': 'One',
+            'profession': 'Key Presser',
+            'date_of_birth': 1989,
+            'hometown': {
+                'name': 'Bogotá',
+                'country': 'Colombia'
+            }
+            
+        }
+    }
+
+    def test_gets_one_simple_property(self):
+
+        dot_notation_getter = DotNotationGetter(self.test_dict)
+        property_to_get = 'brand'
+
+        value_got = dot_notation_getter.get_from_string(property_to_get)
+        self.assertEqual(value_got, 'Ford', 'A simple property was not obtained!')
+
+    def test_gets_a_nested_property(self):
+
+        dot_notation_getter = DotNotationGetter(self.test_dict)
+        property_to_get = 'owner.hometown.country'
+
+        value_got = dot_notation_getter.get_from_string(property_to_get)
+        self.assertEqual(value_got, 'Colombia', 'A nested property was not obtained!')
+
+    def test_gets_the_default_null_value(self):
+
+        dot_notation_getter = DotNotationGetter(self.test_dict)
+        property_to_get = 'does_not_exist'
+
+        value_got = dot_notation_getter.get_from_string(property_to_get)
+        self.assertEqual(value_got, DotNotationGetter.DEFAULT_NULL_LABEL, 'A nested property was not obtained!')
