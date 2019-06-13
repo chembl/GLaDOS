f7ff2c948e7ecfdb499b07964318137807dfccfe
David Mendez <dmendez@ebi.ac.uk>
2019-06-13 15:49:07 +0100

    Tests: move pre-existing utils tests to utils module

--- src/glados/tests/run_tests.py
+++ src/glados/tests/run_tests.py
@@ -10,5 +10,6 @@ if __name__ == "__main__" or __name__ == "glados.tests.run_tests":
     print('starting tests!!')
     from django.core.management import execute_from_command_line
     # 3rd argument is the directory for where to look for tests
+    execute_from_command_line(['runtests.py', 'test', 'glados.utils.tests'])
     execute_from_command_line(['runtests.py', 'test', 'glados.es_properties_configuration.tests'])
     execute_from_command_line(['runtests.py', 'test', 'glados.tests'])
