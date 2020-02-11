88bddd0b85cf8cacbcafc089ed21cb3b446e0d40
David Mendez <dmendez@ebi.ac.uk>
2020-02-11 11:00:24 +0000

    Target Prediction: Fail gracefuly when chembl id has no structure

--- src/glados/api/chembl/target_prediction/service.py
+++ src/glados/api/chembl/target_prediction/service.py
@@ -5,6 +5,11 @@ import requests
 from glados.usage_statistics import glados_server_statistics
 
 
+class TargetPredictionError(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+
 def get_smiles_from_chembl_id(molecule_chembl_id):
 
     index_name = 'chembl_molecule'
@@ -24,13 +29,32 @@ def get_smiles_from_chembl_id(molecule_chembl_id):
     current_hit = hits[0]
     source = current_hit.get('_source')
     molecule_structures = source.get('molecule_structures')
+
+    if molecule_structures is None:
+        raise TargetPredictionError('The compound ' + molecule_chembl_id + ' has no defined structure!')
+
     smiles = molecule_structures.get('canonical_smiles')
+
+    if smiles is None:
+        raise TargetPredictionError('The compound ' + molecule_chembl_id + ' has no defined structure!')
 
     return smiles
 
 
 def get_target_predictions(molecule_chembl_id):
-    smiles = get_smiles_from_chembl_id(molecule_chembl_id)
+
+    try:
+
+        smiles = get_smiles_from_chembl_id(molecule_chembl_id)
+
+    except TargetPredictionError as error:
+
+        final_response = {
+            'predictions': [],
+            'msg': 'No predictions could be returned because of this error: ' + repr(error)
+        }
+        return final_response
+
     external_service_request = requests.post('http://hx-rke-wp-webadmin-04-worker-3.caas.ebi.ac.uk:31112/function/mcp',
                                              json={"smiles": smiles})
 
