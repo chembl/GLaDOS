ae9ad159bf79d563ce69c0f9fd2a7cd7a5e3590f
David Mendez <dmendez@ebi.ac.uk>
2020-02-11 09:39:12 +0000

    Target Prediction: require molecule chembl id for the endpoint

--- src/glados/api/chembl/target_prediction/controller.py
+++ src/glados/api/chembl/target_prediction/controller.py
@@ -3,5 +3,5 @@ from django.views.decorators.http import require_GET
 
 
 @require_GET
-def get_target_prediction(request):
-    return JsonResponse({'hello': 'world'})
+def get_target_prediction(request, molecule_chembl_id):
+    return JsonResponse({'hello': molecule_chembl_id})
