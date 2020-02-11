85ae49c18f9e0ceb7b3329519b11ce44fe9c1866
David Mendez <dmendez@ebi.ac.uk>
2020-02-11 10:47:10 +0000

    Target Prediction: get smiles from elasticsearch

--- src/glados/api/chembl/target_prediction/service.py
+++ src/glados/api/chembl/target_prediction/service.py
@@ -1,10 +1,38 @@
+import json
+
 import requests
+
+from glados.usage_statistics import glados_server_statistics
+
+
+def get_smiles_from_chembl_id(molecule_chembl_id):
+
+    index_name = 'chembl_molecule'
+    es_query = {
+        "_source": [
+            "molecule_structures.canonical_smiles"
+        ],
+        "query": {
+            "terms": {
+                "molecule_chembl_id": [molecule_chembl_id]
+            }
+        }
+    }
+
+    es_response = glados_server_statistics.get_and_record_es_cached_response(index_name, json.dumps(es_query))
+    hits = es_response.get('hits').get('hits')
+    current_hit = hits[0]
+    source = current_hit.get('_source')
+    molecule_structures = source.get('molecule_structures')
+    smiles = molecule_structures.get('canonical_smiles')
+
+    return smiles
 
 
 def get_target_predictions(molecule_chembl_id):
-    smiles = 'C1=CC(=C(C=C1CCN)O)O'
+    smiles = get_smiles_from_chembl_id(molecule_chembl_id)
     external_service_request = requests.post('http://hx-rke-wp-webadmin-04-worker-3.caas.ebi.ac.uk:31112/function/mcp',
-                                              json={"smiles": smiles})
+                                             json={"smiles": smiles})
 
     external_service_response = external_service_request.json()
     final_response = {
