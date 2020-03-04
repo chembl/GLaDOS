c41eb1b545289ae01f2ac10a97d18f0153c6422c
David Mendez <dmendez@ebi.ac.uk>
2020-03-04 17:15:42 +0000

    Target prediction: raise 404 error properly

--- src/glados/api/chembl/target_prediction/controller.py
+++ src/glados/api/chembl/target_prediction/controller.py
@@ -9,6 +9,6 @@ def get_target_prediction(request, molecule_chembl_id):
     agent = request.headers['User-Agent']
     if 'wget' in agent.lower():
         print('Target prediction rejecting wget')
-        return Http404('Not found')
+        raise Http404('Not found')
 
     return JsonResponse(service.get_target_predictions(molecule_chembl_id))
