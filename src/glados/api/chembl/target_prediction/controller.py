3b1b02e7bbef51639708cad3b6384833ad29dfeb
David Mendez <dmendez@ebi.ac.uk>
2020-03-05 16:29:24 +0000

    Try blocking requests that exeed limit

--- src/glados/api/chembl/target_prediction/controller.py
+++ src/glados/api/chembl/target_prediction/controller.py
@@ -7,10 +7,10 @@ from . import service
 
 
 @require_GET
-@ratelimit(key=settings.RATE_LIMIT_KEY, rate='2/m')
+@ratelimit(key=settings.RATE_LIMIT_KEY, rate='2/m', block=True)
 def get_target_prediction(request, molecule_chembl_id):
 
     print('RATE LIMIT KEY: ', settings.RATE_LIMIT_KEY)
-    was_limited = getattr(request, 'limited', False)
-    print('was_limited: ', was_limited)
+    # was_limited = getattr(request, 'limited', False)
+    # print('was_limited: ', was_limited)
     return JsonResponse(service.get_target_predictions(molecule_chembl_id))
