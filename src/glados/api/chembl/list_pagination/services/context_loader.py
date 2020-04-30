50d70d35cd579d3bdb36210633656de1c423161e
David Mendez <dmendez@ebi.ac.uk>
2020-04-30 15:39:29 -0500

    Generate the correct context url

--- src/glados/api/chembl/list_pagination/services/context_loader.py
+++ src/glados/api/chembl/list_pagination/services/context_loader.py
@@ -1,6 +1,7 @@
 """
 Module that loads contexts from results
 """
+from django.conf import settings
 
 
 def get_context_url(context_dict):
@@ -8,4 +9,4 @@ def get_context_url(context_dict):
     returns the url for loading the context
     :param context_dict: dict describing
     """
-    return 'hola'
+    return settings.DELAYED_JOBS_BASE_URL + '/outputs' + '/' + context_dict['context_id'] + '/results.json'
--- src/glados/api/chembl/list_pagination/services/test/test_context_loader.py
+++ src/glados/api/chembl/list_pagination/services/test/test_context_loader.py
@@ -5,7 +5,7 @@ import unittest
 
 from django.conf import settings
 
-# from glados.api.chembl.list_pagination.services import context_loader
+from glados.api.chembl.list_pagination.services import context_loader
 
 
 class TestContextLoader(unittest.TestCase):
@@ -21,6 +21,10 @@ class TestContextLoader(unittest.TestCase):
             'context_type': 'SIMILARITY',
             'context_id': 'STRUCTURE_SEARCH-V4s_piFIe9FL7sOuJ0jl6U0APMEsoV-b4HfFE_dtojc='
         }
-        print('RUNNING TEST!')
-        context_url_must_be = settings.DELAYED_JOBS_BASE_URL
-        print('context_url_must_be: ', context_url_must_be)
+
+        context_url_must_be = settings.DELAYED_JOBS_BASE_URL + '/outputs' + '/' + context_dict[
+            'context_id'] + '/results.json'
+
+        context_url_got = context_loader.get_context_url(context_dict)
+
+        self.assertEqual(context_url_must_be, context_url_got, msg='The context url was not generated correctly!')
